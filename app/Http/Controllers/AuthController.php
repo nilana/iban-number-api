227a8a8ca89003c8a73c885f143ed9e337492753
Nilan Amarasinghe <nilan.a@hotmail.com>
2023-09-02 09:12:37 +0530

    Implemented AuthController login function and added route for login

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -7,6 +7,7 @@ use App\Models\User;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
@@ -41,6 +42,45 @@ class AuthController extends Controller
                 'message' => 'Registration successful'], 
                 201
             );
+    }
+
+    public function login(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'email' => 'required',
+            'password' => 'required',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json(
+                [   
+                    'success' => false,
+                    'message' => $validator->errors()
+                ], 
+                422
+            );
+        }
+        $credentials = $request->only('email', 'password');
+
+        if (Auth::attempt($credentials)) {
+            $user = Auth::user();
+            $token = $user->createToken('auth-token')->plainTextToken;
+
+            return response()->json([
+                'success' => true,
+                'message' => 'Login successful',
+                'data' => [
+                    'token' => $token
+                ],
+            ]);
+        }
+
+        return response()->json([
+            'success' => false,
+            'message' => 'Invalid credentials'
+        ], 
+        401
+        );
     }
 
 }
