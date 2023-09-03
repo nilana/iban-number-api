ac06159f75cdcd51e0f66f0391ea602be2789e32
Nilan Amarasinghe <nilan.a@hotmail.com>
2023-09-03 22:38:17 +0530

    seed sample user data with admin

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,11 +12,13 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        \App\Models\User::factory()->create([
+            'name' => 'Administrator',
+            'email' => 'admin@example.com',
+            'is_admin' => 1,
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password'
+        ]);
+        \App\Models\User::factory(55)->create();
+       
     }
 }
